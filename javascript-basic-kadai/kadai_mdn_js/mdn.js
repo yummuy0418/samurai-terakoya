05b85c9b17672565fd046185ad7e5745bb35a5ea
yummuy0418 <149285501+yummuy0418@users.noreply.github.com>
2023-11-29 21:43:17 +0900

    Delete javascript-basic-kadai/kadai_mdn_js directory

--- javascript-basic-kadai/kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const year = today.getFullYear();
-const month = today.getMonth();
-const date = today.getDate(); 
-  
-  console.log(year + '年' + month + '月'+ date + '日');
