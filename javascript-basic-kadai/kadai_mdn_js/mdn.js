84b2df6429af70267f1ff4433bee0ee4cab16778
yummuy0418 <149285501+yummuy0418@users.noreply.github.com>
2023-11-29 18:10:47 +0900

    Add files via upload

---
+++ javascript-basic-kadai/kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const date = today.getDate(); 
+  
+  console.log(year + '年' + month + '月'+ date + '日');
