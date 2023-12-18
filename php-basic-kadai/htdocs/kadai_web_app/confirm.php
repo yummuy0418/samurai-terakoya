5a75ca080a3b3f9a0222019f3e78e28a997ceca8
yummuy0418 <149285501+yummuy0418@users.noreply.github.com>
2023-12-18 22:51:00 +0900

    Add files via upload

---
+++ php-basic-kadai/htdocs/kadai_web_app/confirm.php
@@ -0,0 +1,46 @@
+<?php
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$dep = $_POST['user_dep'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+  <h2>入力内容をご確認ください。</h2>
+  <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+  <table border="1">
+    <tr>
+      <th>項目</th>
+      <th>入力内容</th>
+    </tr>
+    <tr>
+      <td>社員名</td>
+      <td><?php echo $name; ?></td>
+    </tr>
+    <tr>
+      <td>年齢</hd>
+      <td><?php echo $age; ?></td>
+    </tr>
+    <tr>
+      <td>所属部署</td>
+      <td><?php echo $dep; ?></td>
+    </tr>
+  </table>
+
+  <p>
+  <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+    <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+  </p>
+</body>
+</html>
+
+
+
